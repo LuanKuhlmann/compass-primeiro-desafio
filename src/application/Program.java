c6d9a8dfca8fa7327331c3884f9bf45619e0b657
Luan Campos Kuhlmann <125581971+LuanKuhlmann@users.noreply.github.com>
2023-06-12 21:53:37 -0300

    Incluindo library, testando conexão e Product obj

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,17 @@
 package application;
 
+import db.DB;
 import entities.Product;
+
+import java.sql.Connection;
 
 public class Program {
     public static void main(String[] args) {
 
-        Product obj = new Product(1, "Computer", 2000.0, 2);
-        System.out.println(obj);
+        //Product obj = new Product(1, "Computer", 2000.0, 2);
+        //System.out.println(obj);
+
+        Connection conn = DB.getConnection();
+        DB.closeConnection();
     }
 }
